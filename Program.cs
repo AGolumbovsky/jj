ab2d21180b032553ee0cd6c86a90213e4d4d6193
Andriy Golumbovsky <agolumbovsky@gmail.com>
2019-09-17 22:45:42 -0400

    added switch stmnt for addr

--- Program.cs
+++ Program.cs
@@ -8,20 +8,34 @@ namespace jj
         static void Main(string[] args)
         {
 
-            var adapter = "Wi-Fi"; // args[0];
+            var adapter = "\"Ethernet 8\""; // args[0];
             var sOrD = "static"; // args[1];
-            var addr = args[0];
+            // var addr = args[0];
             var subnet = "255.255.255.0"; // args[3];
             var defGway = "10.10.10.1"; // args[4];
 
-            if (addr == "a")
+            var addr = args[0];
+
+            switch (addr)
             {
-                addr = "192.168.55.55";
+                case "192":
+                    addr = "192.168.1.142";
+                    break;
+
+                case "172":
+                    addr = "172.16.0.142";
+                    break;
+
+                case "10":
+                    addr = "10.10.1.142";
+                    break;
+
+                default:
+                    addr = "8.8.8.8";
+                    break;
             }
-            else if (addr == "b")
-            {
-                addr = "10.55.55.55";
-            }
+
+
 
             var formattedStr = "interface ip set address" + " " + adapter + " " + sOrD + " " + addr;
 
