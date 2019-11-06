77b45527f9f76aeacc0822be9e4cd1f6bef13428
Andriy Golumbovsky <agolumbovsky@gmail.com>
2019-11-05 19:57:30 -0500

    catch up

--- MessWithArgs.cs
+++ MessWithArgs.cs
@@ -20,7 +20,23 @@ namespace jj
         {
             Console.WriteLine("messing with statDyn");
 
-            string statDyn = "static";
+            string statDyn = "";
+
+            switch (statDyn)
+            {
+                case "s":
+                    statDyn = "static";
+                    break;
+
+                case "d":
+                    statDyn = "dhcp";
+                    break;
+
+                default:
+                    statDyn = "dhcp";
+                    break;
+            }
+
             return statDyn;
         }
 
@@ -29,6 +45,31 @@ namespace jj
             Console.WriteLine("messing with addr");
 
             string addr = "7.7.7.7";
+
+            switch (addr)
+            {
+                case "192":
+                    addr = "192.168.1.142";
+                    break;
+
+                case "172":
+                    addr = "172.16.0.142";
+                    break;
+
+                case "10":
+                    addr = "10.10.1.142";
+                    break;
+
+                case "icfff":   //don't show sensitive info in public repo. For now let's just do stars, ok?
+                    addr = "****";
+                    break;
+
+                default:
+                    addr = "8.8.8.8";
+                    break;
+            }
+
+
             return addr;
         }
     }
--- Program.cs
+++ Program.cs
@@ -57,51 +57,6 @@ namespace jj
                     break;
             }
 
-
-            switch (sOrD)
-            {
-                case "s":
-                    sOrD = "static";
-                    break;
-
-                case "d":
-                    sOrD = "dhcp";
-                    addr = " "; 
-                    break;
-
-                default:
-                    sOrD = "dhcp";
-                    break;
-            }
-
-            switch (addr)
-            {
-                case "192":
-                    addr = "192.168.1.142";
-                    break;
-
-                case "172":
-                    addr = "172.16.0.142";
-                    break;
-
-                case "10":
-                    addr = "10.10.1.142";
-                    break;
-                // !!!!!!!! don't leave it like this, it will blow up in your face before you know it
-                case "d":
-                    sOrD = "dhcp";
-                    addr = "";
-                    break;
-
-                case "icfff":   //don't show sensitive info in public repo. For now let's just do stars, ok?
-                    addr = "****";
-                    break;
-
-                default:
-                    addr = "8.8.8.8";
-                    break;
-            }
-
             Process p = new Process();
             ProcessStartInfo psi = new ProcessStartInfo("netsh", queryStr);
             p.StartInfo = psi;
