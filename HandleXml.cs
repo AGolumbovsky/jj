1074af525d8478a5404b64de4445dadfdbfbb035
Andriy Golumbovsky <agolumbovsky@gmail.com>
2019-11-19 22:24:12 -0500

    started with handling xml but it's time to sleep so I'll have to continue later. I wonder is there is a limit on the number of characters...

---
+++ HandleXml.cs
@@ -0,0 +1,23 @@
+﻿
+
+// ALL BElOW CODE IS FROM MS EXAMPLES, MAKE YOUR OWN DAMN CODE !!!!!
+
+using System;
+using System.IO;
+using System.Xml;
+
+public class HandleXml
+{
+    public void SomeFn()
+    {
+        //Create the XmlDocument.
+        XmlDocument doc = new XmlDocument();
+        doc.LoadXml("<?xml version='1.0' ?>" +
+                    "<book genre='novel' ISBN='1-861001-57-5'>" +
+                    "<title>Pride And Prejudice</title>" +
+                    "</book>");
+
+        //Display the document element.
+        Console.WriteLine(doc.DocumentElement.OuterXml);
+    }
+}
