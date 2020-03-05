b9c7f9b74010868718e9fcf94f02d643413a3904
Andriy Golumbovsky <agolumbovsky@gmail.com>
2020-03-05 13:49:36 -0500

    some ip changes that don't affect functionality

--- HandleArgs.cs
+++ HandleArgs.cs
@@ -53,7 +53,7 @@ namespace jj
                     break;
 
                 case "192":
-                    addr = "static 192.168.0.89";
+                    addr = "static 192.168.1.47";
                     break;
 
                 case "172":
